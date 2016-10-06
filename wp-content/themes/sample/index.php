146dade615330e03af4ec2360d71c1e34aa1dfdd
Patricia <poltss@gmail.com>
2016-10-06 11:15:20 -0300

    init project sample 2

---
+++ wp-content/themes/sample/index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+	<main>
+		<section>
+			
+			<div class="row">
+				<div class="col-md-12">
+					<div class="box-content hidden-xs">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/sample-image.jpg">
+					</div>
+					<div class="box-content hidden-md hidden-lg">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/sample-image-mobile.png">
+					</div>
+				</div>
+				
+			</div> 
+		
+		</section> 
+	</main>
+<?php get_footer(); ?>
